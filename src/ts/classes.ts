32a9efecf44ab48a224c6cb352f04a51cf672785
Alessandro Catucci <graphic.alessandro@gmail.com>
2025-04-15 11:20:44 +0200

    added file classes

---
+++ src/ts/classes.ts
@@ -0,0 +1,52 @@
+// import interfaces
+
+import { IProfessionistaMedia, IProgrammaFormazione, IPiattaforma } from "./interfaces";
+
+// create classes
+
+class ProfessionistaMedia implements IProfessionistaMedia {
+    constructor(
+        public nome: string,
+        public cognome: string,
+        public specializzazione: string,
+        public fotoProfilo?: string
+    ) {}
+    partecipaProgramma(programma: IProgrammaFormazione): void {
+        programma.aggiungiPartecipante(this);
+    }
+}
+
+class ProgrammaFormazione implements IProgrammaFormazione {
+    constructor(
+        public nome: string,
+        public descrizione: string,
+        public ambitoSpecializzazione: string,
+        public durata: string,
+        public elencoPartecipanti: IProfessionistaMedia[] = [],
+    ) {}
+    aggiungiPartecipante(professionista: IProfessionistaMedia): void {
+        this.elencoPartecipanti.push(professionista);
+    }
+}
+
+class Piattaforma implements IPiattaforma {
+    constructor(
+        public nome: string,
+        public tipo: string,
+        public descrizione: string,
+        public categoriaContenuto: string[],
+        public elencoContenuti: string[] = [],
+    ) {}
+    pubblicaContenuto(professionista: IProfessionistaMedia, contenuto: string, titoloFormazione: IProgrammaFormazione): void {
+        this.elencoContenuti.push(contenuto);
+        console.log(
+            `Contenuto "${contenuto}"
+            pubblicato da: ${professionista.nome} ${professionista.cognome},
+            per il programma "${titoloFormazione.nome}"
+            della durata di ${titoloFormazione.durata}.
+            Descrizione: ${titoloFormazione.descrizione}
+            Ambito di specializzazione: ${titoloFormazione.ambitoSpecializzazione}
+            `)}
+}
+
+export { ProfessionistaMedia, ProgrammaFormazione, Piattaforma } //export classes
